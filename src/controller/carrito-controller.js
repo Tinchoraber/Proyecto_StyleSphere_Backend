576f1e234bf9eae9bbb88630da9dc2f2eb9d861b
Android Studio <devandroid@ort>
2024-10-21 11:57:22 -0300

    Hicimos carrito, hay que mejorar diseño(el input de cantidad) y validar que la cantidad no se pase del stock

--- src/controller/carrito-controller.js
+++ src/controller/carrito-controller.js
@@ -1,14 +1,49 @@
 import { Router } from 'express';
 import CarritoService from '../service/carrito-service.js';
+import jwt from 'jsonwebtoken';
 const router = Router();
 const svc = new CarritoService();
+const verifyToken = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+    const secretKey = process.env.ACCESS_TOKEN_SECRET || 'ClaveSuperSecreta2006$';
 
-router.post('', async (req, res) => {
-    const idProducto = req.body.idProducto;
-    const idCliente = req.body.idCliente;
-    const returnArray = await svc.insertCarritoAsync(idProducto, idCliente);
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const token = bearer[1];
+
+        jwt.verify(token, secretKey, (err, user) => {
+            if (err) {
+                return res.status(401).json({ message: 'Token inválido' });
+            }
+            req.user = user; 
+            next();
+        });
+    } else {
+        res.status(403).json({ message: 'Token requerido' });
+    }
+};
+router.get('/', verifyToken, async (req, res) => {
+    const idCliente = req.user.idCliente;  
+    const returnArray = await svc.selectCarritoAsync(idCliente);
     res.status(returnArray[1]).json(returnArray[0]);
 
 });
+router.post('', verifyToken, async (req, res) => {
+    const idProducto = req.body.idProducto;
+    const cantidad = req.body.cantidad;
+    const idCliente = req.user.idCliente;
+    const returnArray = await svc.insertCarritoAsync(idProducto, idCliente, cantidad);
+    res.status(returnArray[1]).json(returnArray[0]);
+
+});
+
+router.put('/:idProducto', verifyToken, async (req, res) => {
+    const idProducto = req.params.idProducto;
+    const idCliente = req.user.idCliente; 
+    const cantidad = req.body.cantidad;
+    const returnArray = await svc.updateCarritoAsync(idProducto, idCliente, cantidad);
+    res.status(returnArray[1]).json(returnArray[0]);
+});
+
 
 export default router;
