35e4816986068ca3a6c538f3035abe3e157bf915
Android Studio <devandroid@ort>
2024-10-21 12:13:09 -0300

    cambios en perfil

--- src/controller/perfil-controller.js
+++ src/controller/perfil-controller.js
@@ -1,8 +1,23 @@
 import { Router } from 'express';
 import PerfilService from '../service/perfil-service.js';
+import multer from 'multer';
+import path from 'path';
 const router = Router();
 const svc = new PerfilService();
 
+// Configuración de multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // Carpeta donde se guardan las imágenes
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); // Nombre único para la imagen
+    }
+});
+
+const upload = multer({ storage: storage });
+
+// Middleware para verificar el token
 const verifyToken = (req, res, next) => {
     const bearerHeader = req.headers['authorization'];
     const secretKey = process.env.ACCESS_TOKEN_SECRET || 'ClaveSuperSecreta2006$';
@@ -15,7 +30,7 @@ const verifyToken = (req, res, next) => {
             if (err) {
                 return res.status(401).json({ message: 'Token inválido' });
             }
-            req.user = user; // Asignar el usuario completo al req.user
+            req.user = user;
             next();
         });
     } else {
@@ -23,12 +38,13 @@ const verifyToken = (req, res, next) => {
     }
 };
 
-router.put('/', verifyToken, async (req, res) => {
-    const idCliente = req.params.idCliente
+
+// Endpoint para actualizar el perfil
+router.put('/:idCliente', verifyToken, async (req, res) => {
+    const idCliente = req.params.idCliente;
     const perfil = req.body;
     const returnArray = await svc.actualizarPerfil(perfil, idCliente);
     res.status(returnArray[1]).json(returnArray[0]);
-
 });
 
 export default router;
